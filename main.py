c6eb8fb3d44bb4e951ae4a2d861d40065f1f68db
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-04-08 23:36:22 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from mongoengine import *
+
+connect("tumbledb")
+
+class User(Document):
+    email = EmailField(required=True)
+    first_name = StringField(max_length=50)
+    last_name = StringField(max_length=50)
+
+class Comment(EmbeddedDocument):
+    content = StringField()
+    name = StringField(max_length=120)
+
+class Post(Document):
+    title = StringField(max_length=120, required=True)
+    author = ReferenceField(User, reverse_delete_rule=CASCADE)
+    tags = ListField(StringField(max_length=30))
+    comments = ListField(EmbeddedDocumentField(Comment))
+
+    meta = {"allow_inheritance": True}
+
+class TextPost(Post):
+    content = StringField()
+
+class ImagePost(Post):
+    image_path = StringField()
+
+class LinkPost(Post):
+    link_url = StringField()
+
+
+john = User(email='john@example.com', first_name='john', last_name='Lawley')
+john.save()
+ross = User(email='ross@example.com', first_name='Ross', last_name='Lawley')
+ross.save()
+
+post1 = TextPost(title='Fun with MongoEngine$', author=john)
+post1.content = 'Took a look at MongoEngine today, looks pretty cool.'
+post1.tags = ['mongodb', 'mongoengine']
+post1.save()
+
+post2 = LinkPost(title='MongoEngine Documentation$', author=ross)
+post2.link_url = 'http://docs.mongoengine.com/'
+post2.tags = ['mongoengine']
+post2.save()
+
+for post in Post.objects:
+    print(post.title)
+print()
+for post in TextPost.objects:
+    print(post.title)
