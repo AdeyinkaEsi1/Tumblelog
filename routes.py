6bcd39178a9e63336f86a6850363fec8a6cf5a78
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-04-13 16:55:29 +0100

    new file - routes

---
+++ routes.py
@@ -0,0 +1,41 @@
+from typing import Annotated, List
+from mongoengine import *
+from fastapi import *
+from fastapi.security import *
+from fastapi import HTTPException
+from main import *
+from schemas import *
+from models import *
+from controllers import Controllers
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+router = FastAPI()
+
+router.get("/",
+           status_code=status.HTTP_200_OK
+           )(Controllers.root)
+
+
+
+router.get("/users",
+        response_model=List[UserSchema],
+        status_code=status.HTTP_200_OK
+        )(Controllers.list_users)
+
+
+router.get("/user/{user_id}",
+           status_code=status.HTTP_204_NO_CONTENT
+           )(Controllers.delete_user)
+
+
+router.get("/posts/", response_model=List[PostSchema],
+           status_code=status.HTTP_200_OK,
+           )(Controllers.list_posts)
+
+
+router.post("/post/new",
+          status_code=status.HTTP_201_CREATED,
+          )(Controllers.create_post)
