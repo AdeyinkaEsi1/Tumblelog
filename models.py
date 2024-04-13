2068843df60a678cdffdb97739cdd99adfccadd5
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-04-13 16:55:07 +0100

    new file - models

---
+++ models.py
@@ -0,0 +1,39 @@
+from typing import Annotated, List, Optional
+from mongoengine import *
+from fastapi import *
+from pydantic import BaseModel
+from fastapi.security import *
+from fastapi import HTTPException
+
+
+class User(Document):
+    email = EmailField(required=True)
+    first_name = StringField(max_length=50)
+    last_name = StringField(max_length=50)
+
+
+class Comment(EmbeddedDocument):
+    content = StringField()
+    name = StringField(max_length=120)
+
+
+class Post(Document):
+    title = StringField(max_length=120, required=True)
+    author = ReferenceField(User, reverse_delete_rule=CASCADE)
+    tags = ListField(StringField(max_length=30))
+    comments = ListField(EmbeddedDocumentField(Comment))
+    # content = ListField(EmbeddedDocumentField(TextPost))
+
+    meta = {"allow_inheritance": True}
+
+
+class TextPost(Post):
+    content = StringField()
+
+
+class ImagePost(Post):
+    image_path = StringField()
+
+
+class LinkPost(Post):
+    link_url = StringField()
